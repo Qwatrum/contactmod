efccb4bc157848aac9866b0d0702bff81c98a4dc
Qwatrum <171450552+Qwatrum@users.noreply.github.com>
2026-09-03 12:13:54 +0200

    init commit

---
+++ src/client/java/de/qwatrum/contact/client/ContactClient.java
@@ -0,0 +1,10 @@
+package de.qwatrum.contact.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ContactClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
